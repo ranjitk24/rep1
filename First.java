1127354ae7ef600f87074b950e3a3f5d8744cc2f
ranjitkr <ranjith.kssr5@gmail.com>
2024-09-21 15:36:34 +0530

    updated jav file

--- First.java
+++ First.java
@@ -2,9 +2,11 @@ public class First
 {
 public static void main(String[] args)
 {
-int a=20,b=30,c;
+int a=20,b=30,c,d=30,e=40,f;
 c=a+b;
+f=d*e;
 System.out.println("Hello world");
 System.out.println("the value of c is"+c);
+System.out.println("the value of f is"+f);
 }
 }
